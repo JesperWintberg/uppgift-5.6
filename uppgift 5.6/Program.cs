e4432d9d15034819eda4d3637be216597c958763
Pingvu <nhlpool27@gmail.com>
2023-01-25 11:41:39 +0100

    Add project files.

---
+++ uppgift 5.6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Reflection.Metadata;
+using System.Runtime.Serialization.Formatters;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in dinna tal (tal1 * tal2)");
+            string tal = Console.ReadLine();
+
+
+            string[] split = tal.Split(" * ");
+            int[] tal2 = Array.ConvertAll(split, int.Parse);
+
+            int spara = 1;
+
+            for(int i = 0; i < tal2.Length; i++)
+            {
+
+                spara = tal2[i] * spara;
+
+            }
+
+            Console.WriteLine(spara);
+        }
+    }
+}
